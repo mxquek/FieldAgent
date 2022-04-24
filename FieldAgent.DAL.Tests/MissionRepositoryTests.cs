37dca57ffad99155d66d37c852f84da62fef6554
Mabel Quek <mxquek1@gmail.com>
2022-04-24 14:47:22 -0500

    Created MissionRepo & Tests outline

---
+++ FieldAgent.DAL.Tests/MissionRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using FieldAgent.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldAgent.DAL.Tests
+{
+    public class MissionRepositoryTests
+    {
+        MissionRepository db;
+        DbFactory dbf;
+
+        [SetUp]
+        public void Setup()
+        {
+            ConfigProvider cp = new ConfigProvider();
+            dbf = new DbFactory(cp.Config, FactoryMode.TEST);
+            db = new MissionRepository(dbf);
+            dbf.GetDbContext().Database.ExecuteSqlRaw("SetKnownGoodState");
+        }
+    }
+}
---
+++ FieldAgent.DAL/Repositories/MissionRepository.cs
@@ -0,0 +1,50 @@
+﻿using FieldAgent.Core;
+using FieldAgent.Core.Entities;
+using FieldAgent.Core.Interfaces.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldAgent.DAL.Repositories
+{
+    public class MissionRepository : IMissionRepository
+    {
+        public DbFactory DbFac { get; set; }
+        public MissionRepository(DbFactory dbfac)
+        {
+            DbFac = dbfac;
+        }
+
+        public Response<Mission> Insert(Mission mission)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Update(Mission mission)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Delete(int missionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<Mission> Get(int missionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<List<Mission>> GetByAgency(int agencyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<List<Mission>> GetByAgent(int agentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
